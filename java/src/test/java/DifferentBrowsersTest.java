42817eaa1ae5cecbd1f38fa8d8ff17f01462d77f
Artushkevich Sergey <artushkevichs@gmail.com>
2016-12-15 01:30:13 +0700

    Example of using Internet Explorer. Java

---
+++ java/src/test/java/DifferentBrowsersTest.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class DifferentBrowsersTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new InternetExplorerDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void searchTest() {
+        driver.get("https://duckduckgo.com");
+        wait.until(titleIs("DuckDuckGo"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
